03e3102da9bfb35113856e74463ba48ee6df8216
pmenges88 <pmenges88@gmail.com>
2024-08-26 16:25:01 -0400

    add user authentication express middleware post test

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,24 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-// backend/routes/api/index.js
-// ...
+const { setTokenCookie } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const { restoreUser } = require("../../utils/auth.js");
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
-module.exports = router;
+router.use(restoreUser);
 
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 // backend/routes/index.js
 // ...
 const apiRouter = require('./api');
-
+// GET /api/set-token-cookie
+const { setTokenCookie, restoreUser, requireAuth } = require('../utils/auth.js');
+const { User } = require('../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
 
 
 // Add a XSRF-TOKEN cookie
