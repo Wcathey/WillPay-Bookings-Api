8d6d3422715492356700f9850d080481f304702b
William <wcathey1604@gmail.com>
2024-08-27 22:38:09 -0500

    Add User logout backend endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,15 +6,7 @@ const apiRouter = require('./api');
 // GET /api/set-token-cookie
 const { setTokenCookie, restoreUser, requireAuth } = require('../utils/auth.js');
 const { User } = require('../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
+
 
 
 // Add a XSRF-TOKEN cookie
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -18,9 +18,9 @@ const setTokenCookie = (res, user) => {
       secret,
       { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
-  
+
     const isProduction = process.env.NODE_ENV === "production";
-  
+
     // Set the token cookie
     res.cookie('token', token, {
       maxAge: expiresIn * 1000, // maxAge in milliseconds
@@ -28,7 +28,7 @@ const setTokenCookie = (res, user) => {
       secure: isProduction,
       sameSite: isProduction && "Lax"
     });
-  
+
     return token;
   };
 
@@ -36,12 +36,12 @@ const setTokenCookie = (res, user) => {
     // token parsed from cookies
     const { token } = req.cookies;
     req.user = null;
-  
+
     return jwt.verify(token, secret, null, async (err, jwtPayload) => {
       if (err) {
         return next();
       }
-  
+
       try {
         const { id } = jwtPayload.data;
         req.user = await User.findByPk(id, {
@@ -53,9 +53,9 @@ const setTokenCookie = (res, user) => {
         res.clearCookie('token');
         return next();
       }
-  
+
       if (!req.user) res.clearCookie('token');
-  
+
       return next();
     });
   };
@@ -63,7 +63,7 @@ const setTokenCookie = (res, user) => {
   // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
-  
+
     const err = new Error('Authentication required');
     err.title = 'Authentication required';
     err.errors = { message: 'Authentication required' };
