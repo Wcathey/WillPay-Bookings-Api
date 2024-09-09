cf1cdfbf5993b296620b62e5925c81a3e52c76f0
William <wcathey1604@gmail.com>
2024-09-08 23:31:52 -0500

    established reviews router template

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { Review } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+const router = express.Router();
+
+
+
+
+
+
+module.exports = router;
