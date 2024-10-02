cbb154d6058c96e73fb60429c4ddaca2c47e1851
William <wcathey1604@gmail.com>
2024-10-01 20:20:49 -0500

    wrapped thunk with brackets to read file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+import configureStore from './store/store';
 
 const store = configureStore();
 
