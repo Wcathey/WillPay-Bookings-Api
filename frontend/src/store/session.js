9f233067310a24e4cf85c19f12ef46e6b6fb8613
William <wcathey1604@gmail.com>
2024-10-07 20:08:42 -0500

    added logout action to session

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -54,6 +54,14 @@ export const signUp = (user) => async (dispatch) => {
 
 }
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('api/session', {
+    method: 'DELETE'
+  });
+  dispatch(removeUser());
+  return response;
+}
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
